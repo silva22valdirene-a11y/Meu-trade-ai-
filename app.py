458ae3060f806c83b2697545e881f3e2bc6a82a9
silva22valdirene-a11y <silva22valdirene@gmail.com>
2026-06-27 03:26:41 -0300

    Refactor app.py by cleaning up imports and comments
    Removed unnecessary import statements and comments.

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-import streamlit as st
-import ccxt  # <--- ESSA LINHA É OBRIGATÓRIA
-
-# ... resto do seu código ...
-
-# Quando for fazer a conexão, use:
-exchange = ccxt.mercadobitcoin({
-    'apiKey': api_key,
-    'secret': api_secret,
-})
+exchange = ccxt.mercadobitcoin({'apiKey': api_key, 'secret': api_secret})
